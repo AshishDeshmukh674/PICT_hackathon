966e40bb0013dfb228e2bef7b46eeef5e08c4a4a
Nishchay Bhardwaj <nishchaybhardwaj1.9@gmail.com>
2025-01-25 14:27:54 +0530

    Added Strapi on render

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -79,7 +79,8 @@ const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
     // baseURL: 'https://appointment-booking-strapi.onrender.com/api',
-    baseURL:'http://localhost:1337/api',
+    // baseURL:'http://localhost:1337/api',
+    baseURL:'https://pict-strapi.onrender.com/api',
     headers: {
         'Authorization': `Bearer ${API_KEY}`
     }
